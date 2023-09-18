f00d3b095eefb4997882f0926ed6b8a2135bc036
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2023-09-18 16:03:27 -0300

    🌈 design: modify navbar buttons

--- src/modules/ui/interfaces/Navbar.interface.tsx
+++ src/modules/ui/interfaces/Navbar.interface.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon, MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 import { usePathname } from "next/navigation";
 
 import { ERoutes } from "@/models";
@@ -32,7 +32,7 @@ const Navbar = () => {
         <h1 className="font-bold text-xl flex-1">
           {mobile ? ".ovct" : "Octavio Cossy Torquati"}
         </h1>
-        <nav>
+        <nav className="hidden sm:block">
           <ul className="flex space-x-4">
             <li>
               <Link href={ERoutes.HOME} isCurrent={pathname === ERoutes.HOME}>
@@ -44,7 +44,7 @@ const Navbar = () => {
                 Blog
               </Link>
             </li>
-            <li className="hidden sm:block">
+            <li>
               <Link
                 href={"https://github.com/Octaviocossy/portfolio_2023"}
                 target={"_blank"}
@@ -57,13 +57,22 @@ const Navbar = () => {
           </ul>
         </nav>
       </section>
-      <section className="space-x-4 flex">
-        <Button onClick={handleTheme}>
+      <section className="flex">
+        <Button
+          onClick={handleTheme}
+          className="bg-gray-700 hover:bg-gray-800 dark:bg-yellow-500 dark:border-yellow-800 dark:hover:bg-yellow-600"
+        >
           {theme === "light" ? (
-            <MoonIcon className="w-4 h-4 text-gray-700" />
+            <MoonIcon className="w-4 h-4 text-white" />
           ) : (
             <SunIcon className="w-4 h-4 text-white" />
           )}
+        </Button>
+        <Button
+          onClick={() => console.log("testing")}
+          className="ml-4 block sm:hidden"
+        >
+          <Bars3Icon className="w-4 h-4 text-gray-800 dark:text-white" />
         </Button>
       </section>
     </header>
