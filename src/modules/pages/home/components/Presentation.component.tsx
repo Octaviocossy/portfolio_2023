ad7688deb72a28b2e3d5b6b1c2cfd882a7b441eb
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2023-09-24 19:33:38 -0300

    🎟 Adds presentation picture with better quality

--- src/modules/pages/home/components/Presentation.component.tsx
+++ src/modules/pages/home/components/Presentation.component.tsx
@@ -8,7 +8,7 @@ export const Presentation = () => {
         <h1 className="font-bold text-3xl sm:text-4xl">Fullstack Developer</h1>
         <p>Hola 👋 me llamo Octavio, <span className="font-semibold">ovct</span> para los amigos. Soy programador <span className="decoration-yellow-400 underline dark:decoration-yellow-600">Fullstack {process.env.SENIORITY} especializado en React y NodeJS</span>, <span className="underline decoration-pink-400 dark:decoration-pink-600">UI Designer</span> y <span className="underline decoration-cyan-400 dark:decoration-cyan-600">músico</span>🎹.</p>
       </div>
-      <Image src={Prof_Picture} alt="Picture of the author" className="rounded-full w-48 mb-8 sm:mb-0 sm:w-36" />
+      <Image src={Prof_Picture} alt="Picture of the author" className="rounded-full w-[192px] mb-8 sm:mb-0 sm:w-36" />
     </section>
   );
 };
